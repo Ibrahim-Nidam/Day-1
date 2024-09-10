619c060a092f71f1607fa101078ae2237515ef4a
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2024-09-10 12:17:50 +0100

    commit

--- variables/Challenge 2/index.c
+++ variables/Challenge 2/index.c
@@ -6,7 +6,7 @@ int main(){
     printf("entrer la température en celcius a convertir : ");
     scanf(" %f",&temp);
 
-    k = temp + 273.15;
+    k = temp + 273.15;  
 
     printf("la température en kelvin est : %.2f K",k);
     return 0;
